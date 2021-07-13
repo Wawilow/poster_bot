d742e19ce45069a45d542b9cdb96954edc2a5374
Admin <ujif987@mail.ru>
2021-07-14 01:12:00 +0300

    testing file, he don't have anything interesting

--- text.py
+++ text.py
@@ -1,24 +1,26 @@
 import vk_api
 import time
 
-from modules import XML as moduleXml
+# from modules import XML as moduleXml
 
-XML = moduleXml.XML("settings")
-VK = vk_api.VkApi(token=XML.parsingFile("token"))
 
-groupsId = []
-groupsShortName = ""
-for child in XML.parsingFile("groups", False):
-    groupsShortName += child.text + ","
+def lol():
+    XML = moduleXml.XML("settings")
+    VK = vk_api.VkApi(token=XML.parsingFile("token"))
 
-for group in VK.method("groups.getById", {"group_ids": groupsShortName}):
-    groupsId.append(group["id"] * -1)
+    groupsId = []
+    groupsShortName = ""
+    for child in XML.parsingFile("groups", False):
+        groupsShortName += child.text + ","
 
-del groupsShortName
+    for group in VK.method("groups.getById", {"group_ids": groupsShortName}):
+        groupsId.append(group["id"] * -1)
 
-textPost = XML.parsingFile("textPost")
-intervalPost = int(XML.parsingFile("interval"))
+    del groupsShortName
 
-attachments = [attachment.text for attachment in XML.parsingFile("attachments", False)]
-copyright = XML.parsingFile("copyright")
-v = XML.parsingFile("v")
+    textPost = XML.parsingFile("textPost")
+    intervalPost = int(XML.parsingFile("interval"))
+
+    attachments = [attachment.text for attachment in XML.parsingFile("attachments", False)]
+    copyright = XML.parsingFile("copyright")
+    v = XML.parsingFile("v")
