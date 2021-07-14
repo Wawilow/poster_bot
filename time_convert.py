2edcb028ac7b5701d63349f7e5fe9585e7071a5a
Admin <ujif987@mail.ru>
2021-07-14 07:59:32 +0300

    add next_post time function

--- time_convert.py
+++ time_convert.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+import last_post
 
 
 def time_now():
@@ -89,6 +90,11 @@ def datatime_convert(data, delta=0, delta_hours=0):
 
 def for_vk_post_convert(): #превращает нынешнюю дату в дату+1 день в формате unixtime
     return unixtime_convert(one_day_plus_time(datetime.datetime.now()))
+
+
+def next_post_time(VK, groupId):
+    return [*str(last_post.last_postponed_post(VK, groupId)).split(' ')[0].split('-'),
+     *str(last_post.last_postponed_post(VK, groupId)).split(' ')[1].split(':')]
 
 
 if __name__ == '__main__':
