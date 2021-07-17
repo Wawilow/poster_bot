7a1fe70ef9b7e0b275e7b0de78c709715e4260d0
Admin <ujif987@mail.ru>
2021-07-17 03:08:29 +0300

    after lot of try, it working! god i try to download this fucking image 3 days

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import io
+
+import vk_api
 from last_post import last_postponed_post
 from vk_api import VkApi
 from new_post import time_to_post, new_texts_post
 from time_convert import unixtime_convert
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from random import randrange
+import requests
+import ast
 
 from icecream import ic
+print = ic
 
 def post_make(groupId='204098688', text='\n'):
     main_token = 'ad135a8d6e65aa945e86f32aa44e9fd8f5ce4977a18a8b85a12ac9b3079c991c46699611ff17e7679bff6'
@@ -17,6 +26,85 @@ def post_make(groupId='204098688', text='\n'):
                     )
 
 
+def write_msg(user_id, message, phhoto):
+    if message == 'Готово':
+        write_msg_with_photo(user_id, message, phhoto)
+    else:
+        api.messages.send(user_id=user_id, message=str(message), random_id=randrange(999999999))
+
+
+def write_msg_with_photo(user_id, message, phhoto):
+    upload = vk_api.VkUpload(vk)
+    photo = upload.photo_messages(str(phhoto))
+    owner_id = photo[0]['owner_id']
+    photo_id = photo[0]['id']
+    access_key = photo[0]['access_key']
+    attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+    api.messages.send(user_id=user_id, message=str(message), random_id=randrange(999999999),
+                      attachment=attachment)
+
+
+class Test_Bot:
+    def __init__(self, user_id):
+        print("\nСоздан объект бота!")
+
+        self._USER_ID = user_id
+        begin = ['GO', 'НАЧАТЬ', 'ПОГНАЛИ', 'ПОЕХАЛИ', 'СТАРТ', 'ГО']
+        help = ['HELP', 'ПОМОГИ', 'ПОМОЩ', 'ЧЕГО БЛЯТЬ', 'КОМАНДЫ']
+        stop = ['STOP', 'СТОП', 'АСТАНОВИСЬ']
+        good_bye = ['GOOD BAY', 'ПОКА']
+        self._COMMANDS = [begin, help, stop, good_bye]
+
+    def new_message(self, message, user_id):
+        if message.upper() in self._COMMANDS[0]:#начать
+            return f'Выберити шаблон \n {all_shab_text}'
+        elif message.upper() in self._COMMANDS[1]:#help
+            return f'\n{all_comands}'
+        elif message.upper() in self._COMMANDS[2]:#stop
+            del hu_make[user_id]
+            return f'Все шаблоны очищены'
+        elif message.upper() in self._COMMANDS[3]:#stop
+            return f'абоба'
+        else:
+            # print(message, self._COMMANDS[1], message.lower() == self._COMMANDS[1])
+            return f'У меня нету этой команды, если ты хочешь узнать список команд напиши «help»'
+
+
+def main():
+    global api, vk, longpoll
+    hu_make = {}
+    # 1538369a1ae2c6f51c0bcb22c7a2c1444209b4610a30d33319baea3f59dc975e6d902e28f8d2fa2836818
+    token = 'cb0400ae1b14d0875b4803640297401794c9d0984e0585a5521672c3f9aa60e88c856f5ce2248b640ef60'
+
+    vk = vk_api.VkApi(token=token)
+    longpoll = VkBotLongPoll(vk, 204098688)
+    api = vk.get_api()
+
+    print("Server started")
+
+
+def name_from_id(a):
+    return a
+
+
 if __name__ == '__main__':
-    post_make(groupId='198242788')
+    main()
+    while True:
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                atchs = event.object
+                if atchs:
+                    atchs = ast.literal_eval(str(atchs))
+                    atchs = atchs['message']['attachments']
+                    for atch in atchs:
+                        if atch['type'] == 'photo':
+                            photo = atch['photo']
+
+                            url = photo['sizes'][-1]['url']
+                            print(url)
+                            img = requests.get(url).content
+                            f = io.BytesIO(img)
+                            print([i for i in f])
+                    print()
+                    print('New message:')
 
