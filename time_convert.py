a24e204e7622cfbab5a7089744e09fc1b2fb7b25
Admin <ujif987@mail.ru>
2021-08-25 01:50:22 +0300

    lul

--- time_convert.py
+++ time_convert.py
@@ -7,16 +7,12 @@ def time_now():
     return [*str(now).split(' ')[0].split('-'), *(str(now).split(' ')[1].split('.')[0]).split(':')]
 
 
-def unixtime_convert(date): # превращает обычную дата тайм дату в юникс дату
+def unixtime_convert(date):  # превращает обычную дата тайм дату в юникс дату
     try:
         unixtime = time.mktime(date.timetuple())
         return unixtime
     except:
-        return 'error unitime convert'
-
-
-def for_vk_post_convert(): #превращает нынешнюю дату в дату+1 день в формате unixtime
-    return unixtime_convert(one_day_plus_time(datetime.datetime.now()))
+        return 'error unix time convert'
 
 
 def next_post_time(VK, groupId):
@@ -25,4 +21,6 @@ def next_post_time(VK, groupId):
 
 
 if __name__ == '__main__':
-    pass
+    print(time_now())
+    print([*str(datetime.datetime.now()).split(' ')[0].split('-'), *(str(datetime.datetime.now()).split(' ')[1].split('.')[0]).split(':')])
+    print(time_now() == [*str(datetime.datetime.now()).split(' ')[0].split('-'), *(str(datetime.datetime.now()).split(' ')[1].split('.')[0]).split(':')])
--- time_convert_unittest.py
+++ time_convert_unittest.py
@@ -5,36 +5,9 @@ from time_convert import *
 class Testdata_last_post_unittest(unittest.TestCase):
     def test_date_delta(self):
         # if send small date
-        self.assertEqual(data_time_convert([2011, 11, 4]),
-                         datetime.date(2011, 11, 4),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4], delta_days=3),
-                         datetime.date(2011, 11, 7),
-                         'should be ok')
-        # if send big date
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43]),
-                         datetime.datetime(2011, 11, 4, 2, 3, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_days=2),
-                         datetime.datetime(2011, 11, 6, 2, 3, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_hours=3),
-                         datetime.datetime(2011, 11, 4, 5, 3, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_minutes=4),
-                         datetime.datetime(2011, 11, 4, 2, 7, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_days=1, delta_hours=8),
-                         datetime.datetime(2011, 11, 5, 10, 3, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_hours=8, delta_minutes=5),
-                         datetime.datetime(2011, 11, 4, 10, 8, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_days=1, delta_minutes=15),
-                         datetime.datetime(2011, 11, 5, 2, 18, 43, 0),
-                         'should be ok')
-        self.assertEqual(data_time_convert([2011, 11, 4, 2, 3, 43], delta_days=21, delta_hours=3, delta_minutes=17),
-                         datetime.datetime(2011, 11, 25, 5, 20, 43, 0),
+        self.assertEqual(time_now(),
+                         [*str(datetime.datetime.now()).split(' ')[0].split('-'),
+                          *(str(datetime.datetime.now()).split(' ')[1].split('.')[0]).split(':')],
                          'should be ok')
 
 
